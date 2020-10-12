3ce61833d1cee3796a16a31ece8a818cf7813d66
stephanie rousset <strabon@club-internet.fr>
2020-10-12 18:55:26 +0200

    change Lorem text in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,32 +15,27 @@ puts "Creating galeries"
 
 first_galery = Galerie.create!(name: "Balade poétique", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
   Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
-  Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-  Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.")
+  Maecenas ligula massa, varius a, semper congue, euismod non, mi.")
 first_galery.photo.attach(io: File.open('app/assets/images/balade.jpg'), filename: 'balade.jpg')
 
 second_galery = Galerie.create!(name: "Arbres et histoires", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
   Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
-  Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-  Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.")
+  Maecenas ligula massa, varius a, semper congue, euismod non, mi.")
 second_galery.photo.attach(io: File.open('app/assets/images/arbre.jpg'), filename: 'arbre.jpg')
 
 third_galery = Galerie.create!(name: "Intérieurs chimériques", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
   Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
-  Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-  Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.")
+  Maecenas ligula massa, varius a, semper congue, euismod non, mi.")
 third_galery.photo.attach(io: File.open('app/assets/images/interieur.jpg'), filename: 'interieur.jpg')
 
 fourth_galery = Galerie.create!(name: "Paysages et vagabondages", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
   Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
-  Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-  Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.")
+  Maecenas ligula massa, varius a, semper congue, euismod non, mi.")
 fourth_galery.photo.attach(io: File.open('app/assets/images/vagabondage.jpg'), filename: 'vagabondage.jpg')
 
 fifth_galery = Galerie.create!(name: "Entre Terre et Pierres", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
   Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
-  Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-  Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.")
+  Maecenas ligula massa, varius a, semper congue, euismod non, mi.")
 fifth_galery.photo.attach(io: File.open('app/assets/images/sculpture.jpg'), filename: 'sculpture.jpg')
 
 puts "Galeries done"
