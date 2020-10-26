f3d93802e475f06330a89695d303568350874519
stephanie rousset <strabon@club-internet.fr>
2020-10-26 11:41:52 +0100

    devise customisation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+
 end
