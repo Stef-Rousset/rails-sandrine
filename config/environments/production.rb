ef749e670b068c4204891efd3c552513fd2d6d5b
stephanie rousset <strabon@club-internet.fr>
2020-10-30 15:16:18 +0100

    change in production env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer_default_url_options = { host: 'https://git.heroku.com/sandrine-website.git' }
-  Rails.application.routes.default_url_options[:host] = 'https://git.heroku.com/sandrine-website.git'
+  config.action_mailer_default_url_options = { host: ' https://sandrine-website.herokuapp.com/' }
+  Rails.application.routes.default_url_options[:host] = 'https://sandrine-website.herokuapp.com/'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com'
-    port: 587
-    domain: 'gmail.com'
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
     authentication: 'plain'
-    enable_starttls_auto: true
-    user_name: ENV['GMAIL_USERNAME']
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
 
