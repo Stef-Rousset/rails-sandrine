565e3c114a3ea21f58f0c8d789e2e51295f2d782
stephanie rousset <strabon@club-internet.fr>
2020-10-31 16:43:32 +0100

    erase comments lines

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,20 +1,8 @@
 class GalleriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  skip_before_action :authenticate_user!, only: [ :index]
 
   def index
     @galleries = Gallery.all
-    # @sculptures_gallery = Gallery.find_by(name: "Entre Terre et Pierres")
   end
-
-  # def show
-  #   @gallery = Gallery.find(gallery_params)
-  #   @sculptures_gallery = Gallery.find_by(name: "Entre Terre et Pierres")
-  # end
-
-  # private
-
-  # def gallery_params
-  #   params.require(:gallery).permit(:name,:description, :photo, :art_pieces)
-  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :galleries, only: [:index, :show]  do
+  resources :galleries, only: [:index]  do
     resources :art_pieces, only: [:index, :new, :create, :edit, :update ]
   end
   resources :art_pieces, only: [:destroy]
